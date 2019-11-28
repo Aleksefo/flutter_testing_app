f7960b8f3afedbe5117ba197b7a5c4af813520e2
Aleksefo <aleksefo@gmail.com>
2019-11-28 15:30:57 +0200

    Add separate class for questions

--- lib/main5.dart
+++ lib/main5.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_testing_app/QuizBrain.dart';
 
-import 'Queestion.dart';
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -29,13 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> qBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
   int questionNumber = 0;
 
   @override
@@ -50,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                qBank[questionNumber].questionText,
+                quizBrain.qBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,7 +70,8 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   questionNumber++;
-                  bool correctAnswer = qBank[questionNumber].questionAnswer;
+                  bool correctAnswer =
+                      quizBrain.qBank[questionNumber].questionAnswer;
                   if (correctAnswer) {
                     scoreKeeper.add(
                       Icon(
