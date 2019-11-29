587a50937b2e2905efb7726936b1fb8b1a3a7e7d
Aleksefo <aleksefo@gmail.com>
2019-11-29 15:34:36 +0200

    Update UI and add icons

--- lib/InputScreen.dart
+++ lib/InputScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomHeight = 80;
+const activeColor = Color(0xFF1D1E33);
+const bottomColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,38 +23,85 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
+                    child: ReusableCard(
+                      color: activeColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
+                    child: ReusableCard(
+                      color: activeColor,
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
             Expanded(
-              child: ReusableCard(Color(0xFF1D1E33)),
+              child: ReusableCard(color: activeColor),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
+                    child: ReusableCard(color: activeColor),
                   ),
                   Expanded(
-                    child: ReusableCard(Color(0xFF1D1E33)),
+                    child: ReusableCard(color: activeColor),
                   ),
                 ],
               ),
             ),
+            Container(
+              color: bottomColor,
+              margin: EdgeInsets.only(top: 10),
+            )
           ],
         ));
   }
 }
 
+class IconContent extends StatelessWidget {
+  IconContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  ReusableCard(this.color);
+  ReusableCard({@required this.color, this.cardChild});
 
   final Color color;
+  final Widget cardChild;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -58,6 +110,7 @@ class ReusableCard extends StatelessWidget {
         color: color,
         borderRadius: BorderRadius.circular(5),
       ),
+      child: cardChild,
     );
   }
 }
